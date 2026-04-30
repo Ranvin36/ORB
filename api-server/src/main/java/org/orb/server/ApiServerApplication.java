f56f8d4f033b63b18fc2ea7f9404b4ff03735f24
Ranvin36 <ranvin.20221587@iit.ac.lk>
2026-04-30 23:58:27 +0530

    fix: exclude unused classes

--- api-server/src/main/java/org/orb/server/ApiServerApplication.java
+++ api-server/src/main/java/org/orb/server/ApiServerApplication.java
@@ -2,8 +2,15 @@ package org.orb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 public class ApiServerApplication {
 
 	public static void main(String[] args) {
